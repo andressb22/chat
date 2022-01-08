c0f0c0d42f61091d8a96b4e6f35b19689a3535c0
Andres Sierra <asierra447@gmail.com>
2022-01-08 15:31:50 -0500

    actualizacion de base de datos en puerto

--- database/db.js
+++ database/db.js
@@ -6,9 +6,11 @@ const database = {
     pasword:'pasword12',
     database:'chatall'*/
     host:'ec2-54-80-70-66.compute-1.amazonaws.com',
+    port: '5432',
     user:'osymhlsoanyfxl',
     pasword:'917aee8c9aa24d724025e6ca49471eebe6e5ada685ab6228bc34dffa440bc036',
     database:'d2j39v6dqj9r0g'
+    
 
 }
 const pool = mysql.createPool(database); // pasa el objeto con los datos de la base de datos

4483b8010d191c41cc2e4154fbfb7ee8bed4c86d
Andres Sierra <asierra447@gmail.com>
2022-01-24 23:50:26 -0500

    arreglar nan de los audios prueba 1

--- public/script/emojis.js
+++ public/script/emojis.js
@@ -2,8 +2,6 @@ let emojis = document.querySelector("#emojis");
 let contenedorEmojis = document.querySelector("#contenedorEmojis")
 let contEmoji = document.getElementsByClassName("emoji")
 const chat1 = document.querySelector('#barra-escribir');
-console.log(emojis)
-
 
 for(let i = 0 ; i< contEmoji.length ; i ++){
     contEmoji[i].addEventListener("click",crearEmoji)
@@ -11,13 +9,10 @@ for(let i = 0 ; i< contEmoji.length ; i ++){
 
 
 function crearEmoji(e){
-    console.log(e)
-    console.log(e.target.innerHTML)
     chat1.value += e.target.innerHTML
 }
 emojis.addEventListener("click",function(e){
     contenedorEmojis.style.display == "inline-grid" ? contenedorEmojis.style.display = "none":contenedorEmojis.style.display = "inline-grid"
     contenedorEmojis.style.left = `${e.clientX -330}px`
     contenedorEmojis.style.top = `${e.clientY -345}px`
-    console.log(e);
 })
